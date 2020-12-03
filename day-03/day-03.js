6e4e2840b8d909240ab2bafb88028ebaab5526e3
barhoomi <barhoomi999@outlook.com>
2020-12-03 10:37:08 +0300

    destructure variable assignments

--- day-03/day-03.js
+++ day-03/day-03.js
@@ -2,22 +2,18 @@ const fs = require('fs');
 let input = fs.readFileSync('day-03.txt').toString('utf-8').trim().split('\n');
 input.forEach(x=>x.split(""));
 
-let slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]] //right,down
-let [ans1,ans2] = [0,1];
-
 function findTrees(slope){
-    let [x,y] = [0,0]
-    let trees = 0;
+    let [x,y,trees] = [0,0,0]
     for(y;y<input.length-slope[1];){
-        x+=slope[0];
-        y+=slope[1];
+        [x,y] = [x+slope[0],y+slope[1]]
         x>=input[0].length?x-=input[0].length:0
         input[y][x]=="#"?trees++:0;
     }
     return trees;
 }
 
-ans1 = findTrees(slopes[1]);
+let slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]];
+let [ans1,ans2] = [findTrees(slopes[1]),1]
 slopes.forEach(slope=>{ans2*=findTrees(slope)})
 
 console.log("part 1:",ans1); //230
