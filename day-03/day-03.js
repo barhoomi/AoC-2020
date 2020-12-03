bd07ac118a0497b2b74a5a7812563cb865f4c1f4
barhoomi <barhoomi999@outlook.com>
2020-12-03 09:43:09 +0300

    refactor input and findTrees variables

--- day-03/day-03.js
+++ day-03/day-03.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
-const { totalmem } = require('os');
 let input = fs.readFileSync('day-03.txt').toString('utf-8').trim().split('\n');
-for(i=0;i<input.length;i++){
-    input[i] = input[i].split("");
-}
+input.forEach(x=>x.split(""));
 
 let slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]] //right,down
 let [ans1,ans2] = [0,1];
 
 function findTrees(slope){
-    let currentPos = {x:0,y:0};
+    let [x,y] = [0,0]
     let trees = 0;
-    for(currentPos.y;currentPos.y<input.length-slope[1];){
-        currentPos.x+=slope[0];
-        currentPos.y+=slope[1];
-        if(currentPos.x>=input[0].length){
-            currentPos.x-=input[0].length;
-        }
-        input[currentPos.y][currentPos.x]=="#"?trees++:0;
+    for(y;y<input.length-slope[1];){
+        x+=slope[0];
+        y+=slope[1];
+        x>=input[0].length?x-=input[0].length:0
+        input[y][x]=="#"?trees++:0;
     }
     return trees;
 }
@@ -25,5 +20,5 @@ function findTrees(slope){
 ans1 = findTrees(slopes[1]);
 slopes.forEach(slope=>{ans2*=findTrees(slope)})
 
-console.log("part 1:",ans1);
-console.log("part 2:",ans2);
+console.log("part 1:",ans1); //230
+console.log("part 2:",ans2); //9533698720
